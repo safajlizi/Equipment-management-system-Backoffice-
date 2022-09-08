0e9d00600f0a44a245e148427c5630728f671639
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-09-08 18:31:14 +0100

    Added necessary sql query

---
+++ Visibility.sql
@@ -0,0 +1,19 @@
+
+--
+
+INSERT INTO `equipment_visibility` (`id`, `field`, `visible`) VALUES
+
+('1b4a579e-5bdd-4a6e-b928-d029352816a1', 'deleted_at', 1),
+('2f5f12d9-a1de-4ee1-a34c-8907a9845b51', 'property', 1),
+('3c8867a6-9f12-417d-b4a7-98eed6988218', 'manager', 1),
+('3db4d739-bf18-4e98-a3fe-21c1c42ad815', 'validity_date', 1),
+('3ee30418-c417-464a-b67a-0cd6cbc90451', 'serial_number', 1),
+('4ae0aee0-c25b-4efe-a6b0-2e63697ee8f6', 'updated_at', 1),
+('4efd5bbd-dfad-4e60-bab9-5ce810c7df09', 'category', 1),
+('507a4f72-2d44-4580-a0ed-17f637d741ae', 'created_at', 1),
+('6944ec54-3014-4c50-a4a2-b429f3c416b9', 'availability', 1),
+('b80153a9-1612-431b-8362-5d9ad796684b', 'ref', 1),
+('c472f62b-779a-4857-9a2f-6ef823a913e1', 'label', 1),
+('fb8f2c9d-0f27-42e2-a352-56948daa362b', 'calibration', 1);
+
+
